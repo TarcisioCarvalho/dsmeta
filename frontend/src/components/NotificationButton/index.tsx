cb293f2f67d093aecdfac64e67a1fb7449e56f5f
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-07-13 08:45:31 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notification-meta.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-meta.svg';
+import './styles.css';
 function NotificationButton() {
   return (
     <>
